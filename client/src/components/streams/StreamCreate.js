c1a000dd1053b8fc6981f54a643252761953c872
Artas <artasss47@gmail.com>
2019-03-20 13:21:07 +0100

    create stream form ready to go

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react'
 import { Field, reduxForm} from 'redux-form';
 
 class StreamCreate extends Component {
+
+  renderError = ({touched, error}) => {
+    if(touched && error){
+      return (
+        <div className='ui error message'>
+          <div className='header'>{error}</div>
+        </div>
+      )
+    } 
+    return 
+  }
+
   renderInput = (formProps) => {  
+    const className= `field ${formProps.meta.error && formProps.meta.touched ? 'error' : ''}`
     return (
-      <div className='field'>
+      <div className={className}>
         <label>{formProps.label}</label>
-        <input {...formProps.input}/>
+        <input {...formProps.input} autoComplete='off'/>
+        <div>{this.renderError(formProps.meta)}</div>
       </div>
       )
   }
@@ -18,7 +32,7 @@ class StreamCreate extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form'>
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form error'>
           <Field name='title' component={this.renderInput} label='Enter title'/>
           <Field name='description' component={this.renderInput} label='Enter description'/>
           <button className='ui button'>Submit</button>
@@ -28,6 +42,18 @@ class StreamCreate extends Component {
   }
 }
 
+const validate = (formValues) => {
+  const errors = {}
+  if(!formValues.title){
+    errors.title = 'You must enter a title'
+  }
+  if(!formValues.description){
+    errors.description = 'You must enter a description'
+  }
+  return errors
+}
+
 export default reduxForm({
-  form: 'streamCreate'
+  form: 'streamCreate',
+  validate: validate
 })(StreamCreate);
